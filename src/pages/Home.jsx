a35baa6caac99adc0ae62316e6d35e1f6f0aa5d5
Marcela_Beltran <yulybeltran082@gmail.com>
2022-02-16 18:11:03 -0500

    Se crean las carpetas de los componentes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Carousel } from 'bootstrap';
 import React from 'react'
 import Component1 from '../images/Component1.png';
 import footer from '../images/footer.png';
@@ -8,6 +9,7 @@ const Home = () => {
     <div><body className='App-body'>
     <img className='banner' src={Component1} alt='this is the logo'/> 
     </body>
+   
     <footer className='App-footer'>
     <img className='footer' src={footer} alt='this is the footer'/> 
     </footer></div>
