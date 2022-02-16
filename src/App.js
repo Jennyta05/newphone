0ac04abc0af42866e240d8294ba2f2b05bac0bca
Jennyta05 <hinata.05@hotmail.com>
2022-02-16 17:35:00 -0500

    before add marcela's work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import logo from './images/logo.png';
+import Star from './images/Star.png';
+import Menu from './images/Menu.png';
+import Component1 from './images/Component1.png';
+import footer from './images/footer.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+   <>
+   <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img className='Logo' src={logo} alt='this is the logo'/> 
+      <img className='star' src={Star} alt='this is the fav'/> 
+      <img className='menu' src={Menu} alt='this is the menu'/> 
       </header>
+      <body className='App-body'>
+      <img className='banner' src={Component1} alt='this is the logo'/> 
+      </body>
+      <footer className='App-footer'>
+      <img className='footer' src={footer} alt='this is the footer'/> 
+      </footer>
     </div>
+    </>
   );
 }
 
