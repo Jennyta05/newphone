6a7160eefe8d58d934a98a36f5a5b4699506a3a3
Jennyta05 <hinata.05@hotmail.com>
2022-02-17 04:22:06 -0500

    Menu done

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -7,11 +7,18 @@ import "../styles/navbar.css";
 const Navbar = () => {
   return (
     <div> <header className="App-header">
-    <img className='Logo' src={Logo1} alt='this is the logo'/> 
-    <img className='star' src={Star} alt='this is the fav'/> 
-    <img className='menu' src={Menu} alt='this is the menu'/> 
+      <img className='Logo1' src={Logo1} alt='this is the logo' />
+      <img className='star' src={Star} alt='this is the fav' />
+      <input type="checkbox" id='menu-button' />
+      <label htmlFor="menu-button" className='menu-btn'>
+        <img className='menu' src={Menu} alt='this is the menu' />
+      </label>
+      <ul className='menu-content'>
+         <li className='active'> <a href="#">Usados</a></li>
+         <li> <a href="#">Nuevos</a></li>
+      </ul>
     </header></div>
   )
 }
 
-export  {Navbar}
+export { Navbar }
